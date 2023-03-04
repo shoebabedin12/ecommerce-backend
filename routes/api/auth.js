8acf4e754f26472dc332ac0395a46c88337fa105
shoebabedin12 <shoebabedin12@gmail.com>
2023-03-04 20:42:25 +0600

    new update

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,20 +1,32 @@
 const express = require("express")
-const _ = express.Router()
+const _ = express.Router();
+const User = require("./../../models/user");
 
 _.post("/registration" , (req, res) => {
-   let {email, fullname, password, issubscribe} = req.body;
+   let {email, phoneNumber, firstName, lastName, password} = req.body;
 
    if(!email) {
     return res.json({error: "Please enter a valid email"})
    }
-   if(!fullname) {
-    return res.json({error: "Please enter a valid fullname"})
+   if(!firstName) {
+    return res.json({error: "Please enter a valid firstName"})
+   }
+   if(!lastName) {
+    return res.json({error: "Please enter a valid lastName"})
    }
    if(!password) {
     return res.json({error: "Please enter a valid password"})
    }
    
-   res.json({email, fullname, password, issubscribe});
+   const user = new User({
+      email, 
+      phoneNumber, 
+      firstName, 
+      lastName, 
+      password
+   })
+   user.save();
+   res.json(user);
 })
 
 module.exports = _;
